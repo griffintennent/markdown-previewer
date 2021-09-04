ea9b3d1453aa9c2d6adae7c6c1501425095272ac
griffintennent <griffintennent@gmail.com>
2021-09-04 11:28:25 -0400

    tests all work now, a bunch of misc changes

--- src/features/previewer/Previewer.js
+++ src/features/previewer/Previewer.js
@@ -11,16 +11,23 @@ import DOMPurify from 'dompurify';
 export function Previewer(){
   const markdown = useSelector(selectMarkdown);
   const dispatch = useDispatch();
+  const marked = require('marked');
+
+  marked.setOptions({
+    gfm: true,
+    breaks: true
+  })
 
     return (
       <div id='container'>
+        <div id="titleArea"><h1 id='title'>Markdown Previewer</h1></div>
         <div id='input'>
-          <textarea value={markdown}
+          <textarea id='editor' value={markdown}
             onChange={(e) => dispatch(update(e.target.value))}>
           </textarea>
         </div>
-        <div id='output' 
-        dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(marked(markdown))}} />
+        <div id='preview' dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(marked(markdown))}} 
+        />
       </div>
     );
 }
--- src/features/previewer/previewerSlice.js
+++ src/features/previewer/previewerSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    markdown: '#Here\'s a title\n##Here\'s a smaller title',
+    markdown: '# Here\'s a title\n## Here\'s a smaller title\nLook at this code:\n> ```\n> if(thing.isTrue)\n>   doSomething();\n> ```\n\nYou can also show code inline: `let var = \'You can also show code inline\';`\n\nHere is a link to [Twitter](https://twitter.com)\n\nMarkup is good for **multiple** reasons\n- Fast formatting\n- Easy to understand\n- Many features\n\nHere\'s a logo:\n![logo](logo192.png)\n\nLine Breaks<br>work too!',
 };
 
 export const previewerSlice = createSlice({
